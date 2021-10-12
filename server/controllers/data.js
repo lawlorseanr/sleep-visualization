a0b063443d78dabca94e6a291bef29689d01e132
Sean Lawlor <lawlorseanr@gmail.com>
2021-10-11 22:06:28 -0700

    Implement mock data use to limit calls to S3 endpoint

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -1,15 +1,25 @@
 const DataServer = require("../lib/DataServer");
 
+const USE_MOCK_DATA = true;
+let MOCK_DATA;
+if (USE_MOCK_DATA) {
+  MOCK_DATA = require("../data");
+}
+
 module.exports = (req, res) => {
   const { user_id } = req.params;
 
-  DataServer.get(`/${user_id}.json`)
-    .then((userData) => {
-      const { data } = userData;
-      data.id = user_id;
-      res.status(200).json(data);
-    })
-    .catch((error) => {
-      res.status(404).send(error);
-    });
+  if (USE_MOCK_DATA) {
+    res.status(200).json(MOCK_DATA[user_id]);
+  } else {
+    DataServer.get(`/${user_id}.json`)
+      .then((userData) => {
+        const { data } = userData;
+        data.id = user_id;
+        res.status(200).json(data);
+      })
+      .catch((error) => {
+        res.status(404).send(error);
+      });
+  }
 };
---
+++ server/data/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "2228b530e055401f81ba37b51ff6f81d": require("../data/2228b530e055401f81ba37b51ff6f81d.json"),
+  d6c1355e38194139b8d0c870baf86365: require("../data/d6c1355e38194139b8d0c870baf86365.json"),
+  f9bf229fd19e4c799e8c19a962d73449: require("../data/f9bf229fd19e4c799e8c19a962d73449.json"),
+};
