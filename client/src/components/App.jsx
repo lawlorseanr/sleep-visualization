201f077442dc6aea7e84a7fe74b03f456deb177d
Sean Lawlor <lawlorseanr@gmail.com>
2021-10-16 10:40:29 -0700

    Refactor useEffect implementation to minimize repetetive code

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,55 +2,56 @@ import React, { useState, useEffect } from "react";
 import Plotly from "plotly.js-dist-min";
 import Server from "../lib/Server.js";
 
-const TIMEZONE = 8;
-
 const App = () => {
-  const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState({});
   const [userList, setUserList] = useState([]);
   const [userIndex, setUserIndex] = useState(null);
-  const [intervalIndex, setIntervalIndex] = useState(null);
+  const [intervalIndex, setIntervalIndex] = useState(0);
 
   useEffect(async () => {
     const response = await Server.get("/api/users");
     setUserList(response.data);
+
+    // if userList.length > 0;
+    if (response.data.length > 0) {
+      setUserIndex(0);
+    } else {
+      console.error("No users returned.");
+    }
   }, []);
 
   useEffect(async () => {
-    if (userList.length > 0) {
-      const start_index = 0;
-      const start_interval = 0;
-
-      const response = await Server.get(
-        `/api/data/user/${userList[start_index].id}`
-      );
-
-      console.log(response.data);
-
-      userData[start_index] = response.data;
-      setUserIndex(start_index);
-      setUserData(userData);
-
-      let data = [
-        {
-          x: response.data[start_interval].time,
-          y: response.data[start_interval].data,
-          type: "line",
-        },
-      ];
-
-      let layout = {
-        xaxis: response.data[start_interval].xaxis,
-        yaxis: response.data[start_interval].yaxis,
-        width: 1000,
-        height: 500,
-        margin: {
-          t: 10,
-        },
-      };
-
-      Plotly.newPlot("myChart", data, layout, { displayModeBar: false });
+    if (!userList[userIndex]) {
+      return;
     }
-  }, [userList]);
+
+    const userId = userList[userIndex].id;
+    if (userList[userIndex] && userData[userList[userIndex]] === undefined) {
+      const response = await Server.get(`/api/data/user/${userId}`);
+      userData[userId] = response.data;
+      setUserData(userData);
+    }
+
+    let data = [
+      {
+        x: userData[userId][intervalIndex].time,
+        y: userData[userId][intervalIndex].data,
+        type: "line",
+      },
+    ];
+
+    let layout = {
+      xaxis: userData[userId][intervalIndex].xaxis,
+      yaxis: userData[userId][intervalIndex].yaxis,
+      width: 1000,
+      height: 500,
+      margin: {
+        t: 10,
+      },
+    };
+
+    Plotly.newPlot("myChart", data, layout, { displayModeBar: false });
+  }, [userIndex, intervalIndex]);
 
   return (
     <div id="app">
